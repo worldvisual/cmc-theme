5ffbebdd573e3ec45a4fe8b0cd0f35765899a1b1
Samuel <samuelprado.a@gmail.com>
2022-04-05 20:37:00 -0300

    feat: wp filter input

--- includes/helpers.php
+++ includes/helpers.php
@@ -211,3 +211,21 @@ function get_amazon_lowest_price_product($kw){
 
   return $info;
 }
+
+/*
+|--------------------------------------------------------------------------
+| Filter input
+|--------------------------------------------------------------------------
+*/
+function wp_filter_input(string $filter_type, string $filter_value){
+
+  $filter_type === 'cookie' ? $filter_type = INPUT_COOKIE : $output = null;
+  $filter_type === 'server' ? $filter_type = INPUT_SERVER : $output = null;
+  $filter_type === 'post' ? $filter_type = INPUT_POST : $output = null;
+  $filter_type === 'get' ? $filter_type = INPUT_GET : $output = null;
+  $filter_type === 'env' ? $filter_type = INPUT_ENV : $output = null;
+
+  $output = filter_input($filter_type, $filter_value, FILTER_SANITIZE_STRING);
+
+  return $output ?? 'filter value not found';
+}
