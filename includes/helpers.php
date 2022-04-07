608fb6893f10264d478f5ef41f920fed9e955d12
Samuel <samuelprado.a@gmail.com>
2022-04-06 21:58:49 -0300

    fix: function get_facebook_script

--- includes/helpers.php
+++ includes/helpers.php
@@ -62,14 +62,16 @@ function admin_tips($admin_tip_location) {
 |--------------------------------------------------------------------------
 */
 function get_facebook_script(){
-	return '<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.10&appId=648600791910489";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, "script", "facebook-jssdk"));</script>';
+  ?>
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.10&appId=648600791910489";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, "script", "facebook-jssdk"));</script>
+  <?php
 }
 
 
